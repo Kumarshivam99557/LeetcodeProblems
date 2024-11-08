e6ff0b695ac8ccd05fbc5e475c0d44452912d6c1
Kumar Shivam <thecode.shivam@gmail.com>
2024-11-08 12:07:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (98.49%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int check(char i){
+        if(i=='I'){
+            return 1;
+        }
+        else if(i=='V'){
+            return 5;
+        }
+        else if(i=='X'){
+            return 10;
+        }
+        else if(i=='L'){
+            return 50;
+        }
+        else if(i=='C'){
+            return 100;
+        }
+        else if(i=='D'){
+            return 500;
+        }
+        else if(i=='M'){
+            return 1000;
+        }
+        return 0;
+    }
+    int romanToInt(string s) {
+        int ans=0;
+        for(int i=0; i<s.size(); i++){
+             if(check(s[i])>=check(s[i+1])){
+                ans = ans + check(s[i]);
+             }
+             else {
+                ans = ans - check(s[i]);
+             }
+        }
+        // ans=ans+
+        return ans ;
+    }
+};
